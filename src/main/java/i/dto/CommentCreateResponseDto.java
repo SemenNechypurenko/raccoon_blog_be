e42309279209ffa2ca7f93dd44116c6f56662787
SemenNechypurenko <semen.nechypurenko.gmail.com>
2024-11-26 22:34:15 +0100

    feat renaming

--- src/main/java/i/dto/CommentCreateResponseDto.java
+++ src/main/java/i/dto/CommentCreateResponseDto.java
@@ -7,7 +7,7 @@ public class CommentCreateResponseDto {
     private String id;              // ID комментария
     private String postId;          // ID поста
     private String content;         // Текст комментария
-    private String authorId;        // ID автора
+    private String username;        // ID автора
     private String parentCommentId; // ID родительского комментария
     private String createdAt;       // Дата создания
 }
