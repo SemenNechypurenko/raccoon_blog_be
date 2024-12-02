bc7bf5f76a6515ca8f8bfc5fe02ffb72c83d74fc
SemenNechypurenko <semen.nechypurenko.gmail.com>
2024-12-02 08:41:39 +0100

    feat hide SPRING_DATA_MONGODB_URI

--- src/main/java/i/RaccoonApplication.java
+++ src/main/java/i/RaccoonApplication.java
@@ -1,12 +1,20 @@
 package i;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RaccoonApplication {
 
+    @Value("${spring.data.mongodb.uri}")
+    static String mongoUri;
+
     public static void main(String[] args) {
+
+        String connectingString = mongoUri;
+        System.out.println("        System.out.println(connectingString): " + connectingString);
+
         SpringApplication.run(RaccoonApplication.class, args);
     }
 
