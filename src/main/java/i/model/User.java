ef41ff9880da709dcda7dec11c1d379e41e5378b
SemenNechypurenko <semen.nechypurenko.gmail.com>
2024-12-02 15:08:56 +0100

    fix tests and custom an exception for non EmailNotVerifiedException emails

--- src/main/java/i/model/User.java
+++ src/main/java/i/model/User.java
@@ -8,6 +8,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -32,6 +34,8 @@ public class User {
 
     @NotEmpty(message = "Roles cannot be empty")
     private Set<Role> roles = new HashSet<>();
+
+    private LocalDateTime createdAt = LocalDateTime.now();    // Date and time the user was created
 
     private String confirmationToken;  // Token for email confirmation
 
